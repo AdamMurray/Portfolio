df4e9840491321e6a2545ab64c9fbb36fd5d3bf8
Adam <adam.john.campbell.murray@gmail.com>
2016-10-09 20:19:07 +0100

    Change icon to png

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js').then((registration) => {
       console.log('Service worker registered on scope:', registration.scope);
+      console.log('Service worker reg:', registration);
     })
     .catch((err) => {
       console.log('Service worker registration failed:', err);
