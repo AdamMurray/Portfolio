acd6225e818eee874289fe509a3ea5746b74fc81
Adam <adam.john.campbell.murray@gmail.com>
2016-10-09 22:28:15 +0100

    Update service worker

--- push_notification.sh
+++ push_notification.sh
@@ -5,6 +5,8 @@ server_key=AIzaSyDC6v7Os7_Pq-3j1bE1hh543h8et_T81Ps
 
 # ID of a subscriber (from the subscribe event)
 sub_id=eJtAo7wPS_Q:APA91bFvKhri-A8CGIWQ1Ihp_L_SK1D2_1u-mjrKjXb2h9EU1kWq6RG7TbJsIU_Lp8k0RaaAnWkQHhXuZ6FH0AjBOHbQUt9t6DoXVqLEcL7_faEKF40LPDjuAtdTHJJ0VYLwH4r2Q-SD
+# ID of the app subscription on my phone
+sub_id_2=cix8QPi9wIc:APA91bHE1zzjsni19KEnuwTeiYzexc3Vvd8Rcjnl-XeVJr4XeA1o2OzooLQboNsqDh4HqZRgW4GB-OCjSdu2PmKBg2VeWTr4qzLf-FJztydi-AnhL0DDzMVGg6Q_ACSEVAHjL7zH2RzH
 
 echo
 echo "// ========================= //"
@@ -20,6 +22,6 @@ echo
 echo curl --header "Authorization: key=$server_key" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"$sub_id\"]}"
 echo
 
-curl --header "Authorization: key=$server_key" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"$sub_id\"]}"
+curl --header "Authorization: key=$server_key" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"$sub_id\",\"$sub_id_2\"]}"
 echo
 echo
--- sw.js
+++ sw.js
@@ -171,4 +171,39 @@
       }));
   });
 
+  /**
+   * Push notification click handler
+   */
+  self.addEventListener('notificationclick', function (event) {
+    console.log('Notification click: tag', event.notification.tag);
+    // Android doesn't close the notification when you click it
+    // See http://crbug.com/463146
+    event.notification.close();
+    var url = 'https://adammurray.github.io/Portfolio';
+    // Check if there's already a tab open with this URL.
+    // If yes: focus on the tab.
+    // If no: open a tab with the URL.
+    event.waitUntil(
+      clients.matchAll({
+        type: 'window'
+      })
+        .then((windowClients) => {
+          console.log('WindowClients', windowClients);
+
+          for (var i = 0; i < windowClients.length; i++) {
+            var client = windowClients[i];
+            console.log('WindowClient', client);
+
+            if (client.url === url && 'focus' in client) {
+              return client.focus();
+            }
+          }
+
+          if (clients.openWindow) {
+            return clients.openWindow(url);
+          }
+        })
+    );
+  });
+
 })();
